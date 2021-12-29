71e8a2f204d49e5eb4a606f889189d20e9bad819
farrelarchini14 <farrelarchini14@gmail.com>
2021-12-30 00:25:53 +0800

    User Login Farrel

--- lib/user.dart
+++ lib/user.dart
@@ -1,3 +1,9 @@
+import 'dart:convert';
+import 'package:caseworqer/login.dart';
+import 'package:caseworqer/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class user extends StatefulWidget {
@@ -6,12 +12,129 @@ class user extends StatefulWidget {
 }
 
 class _user extends State<user> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("USER"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('CaseWorqer'),
+        ),
+        body: Padding(
+            padding: EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'CaseWorqer',
+                      style: TextStyle(
+                          color: Colors.green,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 30),
+                    )),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  alignment: Alignment.center,
+                  child: TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      labelText: 'Email',
+                      icon: Icon(Icons.email_outlined),
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      labelText: 'Nama Pengguna',
+                      icon: Icon(Icons.account_circle_outlined),
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      labelText: 'Kata Sandi',
+                      icon: Icon(Icons.password_outlined),
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      labelText: 'Konfirmasi Kata Sandi',
+                      icon: Icon(Icons.password_outlined),
+                    ),
+                  ),
+                ),
+                Container(
+                    margin: EdgeInsets.fromLTRB(20, 35, 20, 35),
+                    height: 50,
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.green,
+                      child: Text('Daftar'),
+                      onPressed: () {
+                        print(nameController.text);
+                        print(passwordController.text);
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => MyStatefulWidget(),
+                            ));
+                      },
+                    )),
+                Container(
+                    margin: EdgeInsets.fromLTRB(20, 35, 20, 35),
+                    child: Row(
+                      children: <Widget>[
+                        Text('Sudah memiliki akun?'),
+                        FlatButton(
+                          textColor: Colors.green,
+                          child: Text(
+                            'Masuk',
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          onPressed: () {
+                            //signup screen
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => LoginPage(),
+                                ));
+                          },
+                        )
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                    ))
+              ],
+            )));
   }
 }
